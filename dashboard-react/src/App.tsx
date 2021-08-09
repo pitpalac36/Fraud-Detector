7caab85fd8eb94bcbf291f08a68c077a170465d5
Laura Dunca <laura_dunca@outlook.com>
2021-08-09 12:46:10 +0300

    styling seems okay, must verify all data is received

--- dashboard-react/src/App.tsx
+++ dashboard-react/src/App.tsx
@@ -3,6 +3,7 @@ import './App.css';
 import Transaction, {processData} from "./Transaction";
 import headers from "./headers";
 import { Table } from 'antd';
+import {MyPagination} from './MyPagination'
 
 
 
@@ -21,25 +22,16 @@ export class App extends React.Component<{}, { endpoint: string, transactions: T
         ws.onopen = () => {
         }
         ws.onmessage = e => {
-            console.log(JSON.parse(e.data))
             this.setState({
                 transactions: this.state.transactions.concat(processData(JSON.parse(e.data)))
             })
         }
     }
 
-    itemRender(current: any, type: string, originalElement: any) {
-        if (type === 'prev') {
-            return <a>Previous</a>;
-        }
-        if (type === 'next') {
-            return <a>Next</a>;
-        }
-        return originalElement;
-    }
-
     render(){
         return(
-            <Table dataSource={this.state.transactions} columns={this.state.columns} pagination={{itemRender: this.itemRender, pageSize: 50, style="padding: 24px"}}/>
+            <Table dataSource={this.state.transactions} columns={this.state.columns}>
+            <MyPagination/>
+            </Table>
         )}
 }
---
+++ dashboard-react/src/MyPagination.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import 'antd/dist/antd.css';
+import './index.css';
+import { Pagination } from 'antd';
+
+export class MyPagination extends React.Component {
+    state = {
+        current: 3,
+    };
+
+    onChange = (page: any) => {
+        console.log(page);
+        this.setState({
+            current: page,
+        });
+    };
+
+    render() {
+        return <Pagination current={this.state.current} onChange={this.onChange} total={50} />;
+    }
+}
